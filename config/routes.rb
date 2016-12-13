83c71ed309f5195598d81c26af7a307f6b83cd1f
darenang44 <darenang44@gmail.com>
2016-12-13 13:58:56 -0800

    Add show and index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pics
   root 'pics#index'
+
+
+
+#   Prefix Verb   URI Pattern              Controller#Action
+#     pics GET    /pics(.:format)          pics#index
+#          POST   /pics(.:format)          pics#create
+#  new_pic GET    /pics/new(.:format)      pics#new
+# edit_pic GET    /pics/:id/edit(.:format) pics#edit
+#      pic GET    /pics/:id(.:format)      pics#show
+#          PATCH  /pics/:id(.:format)      pics#update
+#          PUT    /pics/:id(.:format)      pics#update
+#          DELETE /pics/:id(.:format)      pics#destroy
+#     root GET    /                        pics#index
 end
